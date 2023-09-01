b62d0a8c144f9d730c5d6237f83f8d17ff6c4f4e
Jakob Ruckel <jakob@ruckel.de>
2023-09-01 19:30:30 +0200

    Add SDL2

--- source/hello.cpp
+++ source/hello.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include "glm/common.hpp"
 #include "glm/gtx/norm.hpp"
+#include <SDL2/SDL.h>
 
 int main() {
+    // glm
     glm::vec3 hello{1, 1, 0};
     float dist = glm::distance(glm::vec3{}, hello);
     std::cout << "Hello, " << dist << "!\n";
+
+    // sdl2
+    SDL_Window* win = NULL;
+    SDL_Window* surf = NULL;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Error initializing SDL2\n";
+        return 1;
+    }
+
     return 0;
 }
