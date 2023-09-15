60e02b5f54504cfbf13b85d6a0528a65e9bf2fc0
Jakob Ruckel <jakob@ruckel.de>
2023-09-15 13:09:36 +0200

    Re-format code

--- source/pipeline_builder.h
+++ source/pipeline_builder.h
@@ -5,18 +5,18 @@
 #include <vector>
 
 class PipelineBuilder {
-    public:
-     std::vector<VkPipelineShaderStageCreateInfo> _stages;
-     VkPipelineVertexInputStateCreateInfo _vert_input_info;
-     VkPipelineInputAssemblyStateCreateInfo _input_assembly;
-     VkViewport _viewport;
-     VkRect2D _scissor;
-     VkPipelineRasterizationStateCreateInfo _rasterizer;
-     VkPipelineColorBlendAttachmentState _color_blend_att;
-     VkPipelineMultisampleStateCreateInfo _multisampling;
-     VkPipelineLayout _layout;
+   public:
+    std::vector<VkPipelineShaderStageCreateInfo> _stages;
+    VkPipelineVertexInputStateCreateInfo _vert_input_info;
+    VkPipelineInputAssemblyStateCreateInfo _input_assembly;
+    VkViewport _viewport;
+    VkRect2D _scissor;
+    VkPipelineRasterizationStateCreateInfo _rasterizer;
+    VkPipelineColorBlendAttachmentState _color_blend_att;
+    VkPipelineMultisampleStateCreateInfo _multisampling;
+    VkPipelineLayout _layout;
 
-     VkPipeline build_pipeline(VkDevice device, VkRenderPass pass);
+    VkPipeline build_pipeline(VkDevice device, VkRenderPass pass);
 };
 
 #endif  // PIPELINE_BUILDER_H
